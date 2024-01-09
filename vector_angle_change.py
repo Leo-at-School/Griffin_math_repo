53786b024fd4f7970d65407dbb146370bd55630c
Leo-at-School <153330136+Leo-at-School@users.noreply.github.com>
2024-01-09 11:48:09 -0800

    Update vector_angle_change.py

--- vector_angle_change.py
+++ vector_angle_change.py
@@ -18,7 +18,7 @@ k2 = (slope_2 ** 2) + 1
 angle_1 = asin(sqrt(k1)/k1)
 angle_2 = asin(sqrt(k2)/k2)
 
-rotation = angle_1 - angle_2
+rotation = angle_2 - angle_1
 
 #Printing
 print("(" + str(p1_x) + "," + str(p1_y) + ") to (" + str(p2_x) + "," + str(p2_y) + ")")
