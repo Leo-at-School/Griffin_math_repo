1995b003e0e4442d4293f193001c76a8f4915ed5
Leo-at-School <153330136+Leo-at-School@users.noreply.github.com>
2024-01-09 10:49:59 -0800

    Update vector_angle_change.py

--- vector_angle_change.py
+++ vector_angle_change.py
@@ -1,1 +1,19 @@
-import math
+from math import sqrt, arcsin
+
+p1_x = 3
+p1_y = 4
+p2_x = 6
+p2_y = 10
+
+slope_1 = p1_y/p1_x
+slope_2 = p2_y/p2_x
+
+k1 = (slope_1 ** 2) + 1
+k2 = (slope_2 ** 2) + 1
+
+def cart_to_rad(k)
+    angle = arcsin(sqrt(k)/k)
+
+
+
+
