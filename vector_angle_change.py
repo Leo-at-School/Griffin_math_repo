9b716244a5d11053c6ec1c9faf335931de077815
Leo-at-School <153330136+Leo-at-School@users.noreply.github.com>
2024-01-09 21:45:44 -0800

    Update vector_angle_change.py

--- vector_angle_change.py
+++ vector_angle_change.py
@@ -1,36 +1,38 @@
-from math import sqrt, asin
+from math import atan, pi
 
-#point 1
-p1_x = 3
-p1_y = 4
-#point 2
-p2_x = 6
-p2_y = 10
+solution = []
 
-#points as a slope
+#Example point 1
+p1_x = -2.224
+p1_y = -6.8341
+#Example point 2
+p2_x = 2.6
+p2_y = 5.3225
+
+#Points as a slope
 slope_1 = p1_y/p1_x
 slope_2 = p2_y/p2_x
 
-#Finding angle rotation (in radians)
-k1 = (slope_1 ** 2) + 1
-k2 = (slope_2 ** 2) + 1
+angle_1 = (180/pi) * atan(slope_1)
+angle_2 = (180/pi) * atan(slope_2)
 
-if (p1_y > 0 and p2_y > 0) or (p1_y < 0 and p2_y < 0):
-    angle_1 = asin(sqrt(k1)/k1)
-    angle_2 = asin(sqrt(k2)/k2)
+if angle_1 < 0:
+    angle_1 = -1 * angle_1 + 90
 
-elif (p1_y < 0 and p2_y > 0) or (p1_y > 0 and p2_y < 0):
-    angle_1 = asin(-sqrt(k1)/k1)
-    angle_2 = asin(-sqrt(k2)/k2)
+elif p1_y < 0:
+    angle_1  = -1 * angle_1 - 90
 
-else:
-  angle_1 = 0
-  angle_2 = 0
+if angle_2 < 0:
+    angle_2 = -1 * angle_2 + 90
 
-rotation = angle_2 - angle_1
+elif p2_y < 0:
+    angle_2  = -1 * angle_2 - 90
 
-#Printing
-print("(" + str(p1_x) + "," + str(p1_y) + ") to (" + str(p2_x) + "," + str(p2_y) + ")")
-print("rotation: " + str(rotation))
+solution.append(angle_1)
+solution.append(angle_2)
 
-
+print()
+print("point 1: (" + str(p1_x) + "," + str(p1_y) + ") ⇒ " + str(solution[0]) + " degrees off the x axis")
+print("Point 2: (" + str(p2_x) + "," + str(p2_y) + ") ⇒ " + str(solution[1]) + " degrees off the x axis")
+print()
+print("Point 2 is rotated " + str(solution[1] - solution[0]) + " degrees relative to point 1")
